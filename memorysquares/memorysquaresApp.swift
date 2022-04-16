2701192f3eac3388d958a8adf58b06c19da342c4
Tyler Reed <tylerreedytlearning@gmail.com>
2022-04-15 22:22:34 -0400

    Initial Commit

---
+++ memorysquares/memorysquaresApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorysquaresApp.swift
+//  memorysquares
+//
+//  Created by Tyler Reed on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct memorysquaresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
